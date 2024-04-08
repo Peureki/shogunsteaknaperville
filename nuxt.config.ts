d6663cc6fea77fb7e671c3aae7c8b40207ee34ff
Matt Nguyen <newyenmatt@gmail.com>
2024-04-08 16:19:46 -0500

    NuxtImg test

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,6 +6,7 @@ export default defineNuxtConfig({
     '@nuxt/image',
   ],
   image: {
-    provider: 'ipx'
+    provider: 'netlify',
+    domains: ['https://sparkly-chaja-627a17.netlify.app/']
   }
 })
