ac71d314619533e53c6bafab8ec31ba60ef4cac3
Matt Nguyen <newyenmatt@gmail.com>
2024-04-17 12:57:21 -0500

    Recaptcha

--- plugins/google-recaptcha.ts
+++ plugins/google-recaptcha.ts
@@ -2,7 +2,7 @@ import { VueReCaptcha } from 'vue-recaptcha-v3';
 
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(VueReCaptcha, {
-        siteKey: '6Le6_LUpAAAAAKfoaNmXzB0mJGFKJjfAj6JVCIo-',
+        siteKey: '6Ld1fL4pAAAAAOcVpWOisHfy1bavRXwcir6plC8e',
         loaderOptions: {
             autoHideBadge: true,
         }
